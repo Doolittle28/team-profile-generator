2bb0f26fe04388000ca030e69020b38ed27ab297
Kevin Doolittle <kldpd22@gmail.com>
2022-05-07 17:38:27 -0700

    initial commit with classes built

---
+++ team-generator/lib/Manager.js
@@ -0,0 +1,19 @@
+const employee = require("./employee");
+
+class manager extends employee {
+    constructor(name, id, email, office){
+        super(name, id, email)
+
+        this.office = office;
+    }
+
+
+getOffice(){
+    return this.office;
+}
+
+getRole(){
+    return "Manager";
+}
+
+}
